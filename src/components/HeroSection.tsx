f5baf89c94a65e736c8dca295a44442f644049a6
mmmizanahmed72-source <mmmizanahmed72@gmail.com>
2025-12-20 02:04:47 -0800

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
             animate={{ scale: 1 }}
             transition={{ duration: 0.8, delay: 0.5 }}
           >
-            Next-Gen Developer
+            MD BADHON
           </motion.h1>
           
           <motion.p
